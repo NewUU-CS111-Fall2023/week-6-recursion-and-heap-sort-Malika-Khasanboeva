e6be1af94a0ebfd00cb4dcbc7d0ad69f87b271fa
Malika-Khasanboeva <127714681+Malika-Khasanboeva@users.noreply.github.com>
2023-11-23 22:30:29 +0500

    Problem 2

---
+++ src/task_2.cpp
@@ -0,0 +1,27 @@
+// Time complexity: O(log n)
+#include <iostream>
+
+using namespace std;
+
+bool isPowerOfTwo(int n) {
+    if (n == 1) {
+        return true;
+    } else if (n % 2 != 0 || n == 0) {
+        return false;
+    } else {
+        return isPowerOfTwo(n / 2);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (isPowerOfTwo(n)) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
